8e29ab8eeb3773cae65c9cfb3edeb3d7e2a9c2d4
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2018-01-19 18:58:17 +0100

    Some updates

--- src/js-essential.js
+++ src/js-essential.js
@@ -1,6 +1,6 @@
 export { default as Arrays } from './main/Arrays';
-export { default as Config } from './main/Config';
-export { default as ConfigError } from './main/ConfigError';
+//export { default as Config } from './main/Config';
+//export { default as ConfigError } from './main/ConfigError';
 export { default as Functions } from './main/Functions';
 export { default as Objects } from './main/Objects';
 export { default as Seq } from './main/Seq';
